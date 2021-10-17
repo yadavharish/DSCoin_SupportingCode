7c0798c0ee7922ac6a2e1ee774a097a89560eed0
Harish Yadav <51262690+yadavharish@users.noreply.github.com>
2021-10-17 22:51:29 +0530

    Add files via upload

---
+++ TransactionQueue.java
@@ -0,0 +1,20 @@
+package Includes;
+
+public class TransactionQueue {
+
+  public Transaction firstTransaction;
+  public Transaction lastTransaction;
+  public int numTransactions;
+
+  public void AddTransactions (Transaction transaction) {
+
+  }
+  
+  public Transaction RemoveTransaction () throws EmptyQueueException {
+
+  }
+
+  pulbic int size() {
+
+  }
+}
